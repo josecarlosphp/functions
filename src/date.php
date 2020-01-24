ae0663020d42c999534fb539e6af33f43ded7709
josecarlosphp <38591375+josecarlosphp@users.noreply.github.com>
2020-01-24 21:09:26 +0100

    Cambiar en los comentarios: josecarlosphp/db Por: josecarlosphp/functions

--- src/date.php
+++ src/date.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of josecarlosphp/functions - Common use functions.
  *
- * josecarlosphp/db is free software: you can redistribute it and/or modify
+ * josecarlosphp/functions is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
